b5104595151e944f815a4f4490f64884e2ff3495
LPF <1092156308@qq.com>
2018-11-30 19:18:34 +0800

    add txt

---
+++ management/src/components/TeamManage/Entry.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Entry"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ management/src/components/TeamManage/TeamHumanbeing.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TeamHumanbeing"
+    }
+</script>
+
+<style scoped>
+
+</style>
